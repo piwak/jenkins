5b44aaaa40cae75cf161ee7c705128c357fa1ea2
piwak <piwak90@gmail.com>
2013-05-23 20:37:30 +0200

    Initial commit

---
+++ src/main/java/pl/mp/piwak/maven/MyClass.java
@@ -0,0 +1,11 @@
+package pl.mp.piwak.maven;
+
+import java.util.*;
+
+public class MyClass
+{
+	public static void main(String[] args)
+	{
+		List<String> stringList = new ArrayList<String>();
+	}
+}
